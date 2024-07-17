01407139f6910c6d28bc669e0976912f9421ce9e
giber9g <amir.gibadullin228000@gmail.com>
2024-07-18 01:06:55 +0300

    Resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style/main.scss';
+import Footer from './layout/footer';
+import Header from './layout/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
