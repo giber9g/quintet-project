d563bcf056ce399a5f68541fdcdcf68f172b31f1
quma77 <qumaprogram@gmail.com>
2024-07-20 20:58:56 +0300

    name

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+// import сюда надо scss подключить, как понял main надо
 
-const Header =  () => {
-
-    return (
-        <div className="header">
-            <h1>header</h1>
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="header">
+      <div>logo</div>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Услуги</a>
+          </li>
+          <li>
+            <a href="#">Цены</a>
+          </li>
+          <li>
+            <a href="#">Портфолио</a>
+          </li>
+          <li>
+            <a href="#">Контакты</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>+7(927)450-11-21</p>
+      </div>
+      <div>
+        <button>Связь</button>
+      </div>
+    </div>
+  );
+};
 export default Header;
